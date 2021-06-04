37e148339a0498dd5001cb856bcf06835ce44138
3045542591 <84435197+3045542591@users.noreply.github.com>
2021-06-04 13:50:05 +0800

    Add files via upload

---
+++ lesson3/src/tcp_server.rs
@@ -0,0 +1,42 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 60]; // 使用60字节缓冲区
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() {
+    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
+    // 接受连接并处理它们，为每个连接产生一个新线程
+    println!("Server listening on port 3333");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // 连接成功
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* 连接失败 */
+            }
+        }
+    }
+    // 关闭 socket 服务
+    drop(listener);
+}
